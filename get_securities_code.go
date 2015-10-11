ccc55115a326291c03accf029c4c6849d2451042
t-fukui <fukuimone@gmail.com>
2015-10-11 20:38:17 +0900

    Refactoring get_securities.go

--- get_securities_code.go
+++ get_securities_code.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-     "os"
-     "github.com/PuerkitoBio/goquery"
+    "os"
+    "fmt"
+    "github.com/PuerkitoBio/goquery"
 )
 
 func Exists(name string) bool {
     _, err := os.Stat(name)
     return !os.IsNotExist(err)
+}
+
+type Company struct {
+    code  string
+    brand string
+    name  string
+}
+
+func check(err error) {
+    if err != nil {
+        panic(err)
+    }
 }
 
 func main() {
@@ -18,16 +31,21 @@ func main() {
 
     if Exists("code.txt") {
         file, err = os.Open("code.txt")
+        check(err)
     } else {
         file, err = os.Create("code.txt")
+        check(err)
     }
 
     defer file.Close()
 
-    if err != nil {
-        panic(err)
-    }
-
     doc, _ := goquery.NewDocument(url)
-    doc.Find(".cmn-charcter").Text()
+    doc.Find(".cmn-charcter").Each(func(_ int, ele *goquery.Selection) {
+        code := ele.Find("td.cmn-stock_border").Text()
+        brand := ele.Find("td.cmn-character cmn-stock_border a").Text()
+        name := ele.Find("td.cmn-character cmn-stock_border").Last().Text()
+
+        data := Company{ code, brand, name, }
+        fmt.Println(data)
+    })
 }
