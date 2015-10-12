5eecc639babcd9ea645b58fc4d227b9ccaea289f
t-fukui <fukuimone@gmail.com>
2015-10-12 13:48:00 +0900

    Delete get_securities_code.go file

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+    "os"
     "fmt"
     "strings"
-    "io/ioutil"
     "regexp"
+    "io/ioutil"
     "github.com/PuerkitoBio/goquery"
 )
 
@@ -14,19 +15,40 @@ func check(err error) {
     }
 }
 
+func getNikkeiAve() {
+    url := "https://indexes.nikkei.co.jp/nkave/index/component?idx=nk225"
+
+    os.Remove("code.txt")
+    file, err := os.OpenFile("code.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+    check(err)
+
+    defer file.Close()
+    doc, _ := goquery.NewDocument(url)
+    doc.Find(".cmn-table > tbody").Each(func(_ int, ele1 *goquery.Selection) {
+        doc.Find("tr.cmn-charcter").Each(func(_ int, ele2 *goquery.Selection) {
+            code := strings.TrimSpace(ele2.Find("td.cmn-stock_border").First().Text())
+            brand := strings.TrimSpace(ele2.Find("td.cmn-stock_border a").Text())
+            name := strings.TrimSpace(ele2.Find("td.cmn-stock_border").Last().Text())
+            row := fmt.Sprintf("%s, %s, %s\n", code, brand, name)
+            file.WriteString(row)
+        })
+    })
+}
+
 func getPage(securities_code string) {
     url := fmt.Sprintf("http://stocks.finance.yahoo.co.jp/stocks/detail/?code=%s", securities_code)
     doc, _ := goquery.NewDocument(url)
     doc.Find("div#detail.marB6").Each(func(_ int, s1 *goquery.Selection) {
         s1.Find("div.innerDate").Each(func(_ int, s2 *goquery.Selection) {
             s2.Find("div.lineFi.clearfix").Each(func(_ int, s3 *goquery.Selection) {
-                fmt.Print(s3.Text())
+                fmt.Println(s3.Text())
             })
         })
     })
 }
 
 func main() {
+    getNikkeiAve()
     date, err := ioutil.ReadFile("./code.txt")
     check(err)
     r := regexp.MustCompile(`[0-9]+`)
