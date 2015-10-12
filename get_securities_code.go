d0dfd6a9bdcab95e398b5f1080340194888b5cd9
t-fukui <fukuimone@gmail.com>
2015-10-12 12:36:03 +0900

    日経の証券コードを取得する

--- get_securities_code.go
+++ get_securities_code.go
@@ -3,14 +3,9 @@ package main
 import (
     "os"
     "fmt"
+    "strings"
     "github.com/PuerkitoBio/goquery"
 )
-
-type Company struct {
-    code  string
-    brand string
-    name  string
-}
 
 func check(err error) {
     if err != nil {
@@ -21,18 +16,18 @@ func check(err error) {
 func main() {
     url := "https://indexes.nikkei.co.jp/nkave/index/component?idx=nk225"
 
-    file, err := os.OpenFile("code.txt", os.O_WRONLY|os.O_CREATE, 0600)
+    file, err := os.OpenFile("code.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
     check(err)
 
     defer file.Close()
-
     doc, _ := goquery.NewDocument(url)
-    doc.Find(".cmn-charcter").Each(func(_ int, ele *goquery.Selection) {
-        code := ele.Find("td.cmn-stock_border").Text()
-        brand := ele.Find("td.cmn-character cmn-stock_border a").Text()
-        name := ele.Find("td.cmn-character cmn-stock_border").Last().Text()
-
-        data := Company{ code, brand, name, }
-        fmt.Println(data)
+    doc.Find(".cmn-table > tbody").Each(func(_ int, ele1 *goquery.Selection) {
+        doc.Find("tr.cmn-charcter").Each(func(_ int, ele2 *goquery.Selection) {
+        code := strings.TrimSpace(ele2.Find("td.cmn-stock_border").First().Text())
+        brand := strings.TrimSpace(ele2.Find("td.cmn-stock_border a").Text())
+        name := strings.TrimSpace(ele2.Find("td.cmn-stock_border").Last().Text())
+        row := fmt.Sprintf("%s, %s, %s\n", code, brand, name)
+        file.WriteString(row)
+        })
     })
 }
